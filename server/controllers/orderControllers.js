9a41e91857cf2dcf0091d69c1c1c59c1f9d84cb7
Tejas <tejasnanoti2@gmail.com>
2024-04-07 00:03:54 +0530

    feat(server): Add Invoicing and mailing extension at postOrder;

--- server/controllers/orderControllers.js
+++ server/controllers/orderControllers.js
@@ -1,4 +1,7 @@
 import Order from "../models/orderSchema.js";
+import easyinvoice from 'easyinvoice';
+import getDate from "../utils/getDate.js";
+import nodemailer from 'nodemailer'
 
 export const getOrders = async (req, res) => {
     try {
@@ -19,12 +22,74 @@ export const postOrder = async (req, res) => {
     try {
         const { fullname, phoneNumber, items, subtotal } = req.body;
 
-        await Order.create({
+        const newOrder = await Order.create({
             fullname,
             phoneNumber,
             items,
             subtotal
         })
+
+        var data = {
+            "client": {
+                "company": fullname,
+            },
+            "sender": {
+                "company": "Tejas Nanoti",
+            },
+            "images": {
+                "logo": "https://public.easyinvoice.cloud/img/logo_en_original.png"
+            },
+            "information": {
+                "number": `#${newOrder._id}`,
+                "date": getDate(),
+            },
+            "products": items.map(item => { return { "quantity": item.quantity, "price": item.price, "description": item.name } }),
+            "bottomNotice": "Thank You for shopping with invoiced!",
+            "settings": {
+                "currency": "INR",
+            }
+        };
+
+        console.log(data);
+
+        easyinvoice.createInvoice(data, function (result) {
+            // fs.writeFileSync("invoice.pdf", result.pdf, 'base64');
+
+            const senderMail = process.env.SENDER_MAIL
+            const password = process.env.APP_SPECIFIC_PASSWORD
+
+            console.log(senderMail);
+
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: senderMail,
+                    pass: password
+                }
+            });
+
+            const mailOptions = {
+                from: senderMail,
+                to: senderMail,
+                subject: 'Invoice for your order at Invoiced',
+                text: `Please find the attached invoice below. Invoice #${newOrder._id}`,
+                attachments: [{
+                    filename: 'invoice.pdf',
+                    content: result.pdf,
+                    encoding: 'base64'
+                }]
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                    res.status(500).json({ message: "Error sending email" });
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    res.status(200).json({ message: "Email sent successfully" });
+                }
+            });
+        });
 
         res
             .status(200)
---
+++ server/utils/getDate.js
@@ -0,0 +1,18 @@
+export default function getDate () {
+    var currentDate = new Date();
+
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+
+    if (day < 10) {
+        day = '0' + day;
+    }
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    var formattedDate = day + '/' + month + '/' + year;
+
+    return formattedDate
+}
