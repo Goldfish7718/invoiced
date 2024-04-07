6d1d495e5e86705af274bdccfcbdc714134c63e5
Tejas <tejasnanoti2@gmail.com>
2024-04-07 11:58:51 +0530

    feat(server): Add SMS for orders;

--- server/controllers/orderControllers.js
+++ server/controllers/orderControllers.js
@@ -2,6 +2,7 @@ import Order from "../models/orderSchema.js";
 import easyinvoice from 'easyinvoice';
 import getDate from "../utils/getDate.js";
 import nodemailer from 'nodemailer'
+import twilio from 'twilio'
 
 export const getOrders = async (req, res) => {
     try {
@@ -51,13 +52,9 @@ export const postOrder = async (req, res) => {
             }
         };
 
-        console.log(data);
-
         easyinvoice.createInvoice(data, function (result) {
             const senderMail = process.env.SENDER_MAIL
             const password = process.env.APP_SPECIFIC_PASSWORD
-
-            console.log(senderMail);
 
             const transporter = nodemailer.createTransport({
                 service: 'gmail',
@@ -81,6 +78,7 @@ export const postOrder = async (req, res) => {
 
             transporter.sendMail(mailOptions, function (error, info) {
                 if (error) {
+                    console.log(error);
                     res.status(500).json({ message: "Error sending email" });
                 } else {
                     res.status(200).json({ message: "Email sent successfully" });
@@ -88,11 +86,28 @@ export const postOrder = async (req, res) => {
             });
         });
 
+        const accountSid = process.env.TWILIO_ACCOUNT_SID
+        const auth = process.env.TWILIO_AUTH
+        const twilioPhone = process.env.TWILIO_PHONE
+
+        console.log(auth, accountSid);
+
+        const client = twilio(accountSid, auth)
+
+        client.messages
+            .create({
+                body: `\n\nHello ${fullname}! Thank you for shopping with invoiced! Your order number is #${newOrder._id} with a subtotal of \u20B9${subtotal} containing the following items.\n\n${items.map(item => `${item.name} (${item.quantity})\n`)}\n\nThank you and visit again!`,
+                from: twilioPhone,
+                to: `+91${phoneNumber}`
+            })
+            .then(message => console.log(message.sid))
+
         res
             .status(200)
             .json({ message: "Order saved successfully" })
 
     } catch (error) {
+        console.log(error);
         res
             .status(500)
             .json({ message: "Internal Server Error" })
